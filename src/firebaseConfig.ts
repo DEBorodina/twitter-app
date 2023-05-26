265a757a566618d958410c909add6f80c4253102
daria_borodina <dasha.borodina2003@mail.ru>
2023-05-26 19:01:58 +0300

    feat: add env variaables

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -5,13 +5,14 @@ import 'firebase/compat/database';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC1iMe6pM1o6v32IJqcSQ-3PSnp4unIgAs',
-  authDomain: 'twitter-app-5d2fa.firebaseapp.com',
-  databaseURL: 'https://twitter-app-5d2fa-default-rtdb.firebaseio.com',
-  projectId: 'twitter-app-5d2fa',
-  storageBucket: 'twitter-app-5d2fa.appspot.com',
-  messagingSenderId: '873625730417',
-  appId: '1:873625730417:web:eabea9dd6e6f1d89ea0ad0',
+  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_REACT_APP_DATABASE_URL,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_APP_ID,
+  measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID,
 };
 
 export const app = firebase.initializeApp(firebaseConfig);
